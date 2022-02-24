6a900114c2c9be6532749e14c11e351c14aa0ce8
Mark Russel Miranda <sablay296@gmail.com>
2022-02-24 19:37:41 +0800

    webapp package

--- webapp/main.go
+++ webapp/main.go
@@ -9,8 +9,14 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Welcome To AWESOMENESS!!!!</h1>")
 }
 
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "hello World!!!")
+}
+
 func main() {
 	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/hello", helloWorld)
 	fmt.Println("Starting the server om : 3000 ...")
 	http.ListenAndServe(":3000", nil)
+
 }
